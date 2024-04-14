6d7c80ccfd2d33168325a8c26cf471612dad9081
NNatha05 <nathan.nelen@inraci.be>
2024-04-15 00:22:35 +0200

    amélioration du programme
    j'ai rajouté des ligne pour l'encodeur, pour la fonction que monsieur Mazzeo voulait et  c'est l'addition des mesures, etc.

--- programme.ino
+++ programme.ino
@@ -1,55 +1,125 @@
+
+///////////////////////////////////////////////////////////////////////////
+// Prénom et Nom : Nelen Nathan
+// Date : 25|06|2024
+// Classe : 6ème année secondaire Technique de qualification en électronique
+// Projet : La station météo
+// Matériel : ESP32, encodeur, BME680, BMP390L,
+///////////////////////////////////////////////////////////////////////////
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
+#include "Adafruit_BMP3XX.h"
 
-const char* ssid = "#LaboD5";
+  const char* ssid = "#LaboD5";
 const char* password = "0123456789";
 
-WebServer server(80);
+WebServer serveur(80);
 
 // Capteur BME680
 Adafruit_BME680 bme;
-// on est ici
-// et là on là bas 
+
+// Capteur BMP3XX
+Adafruit_BMP3XX bmp;
+
 // Altitude de référence pour le calcul de l'altitude
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define PRESSIONNIVEAUSEALE (1013.25)
 
-const int led = 13;
+const int brocheEntree = 6;                   // Pin où se trouve le signal carré de l'encodeur
+volatile unsigned long compteImpulsions = 0;  // Variable pour compter le nombre de fronts montants
+const float facteur = 1.0 / 2.0;              // Facteur de conversion pour convertir les pulses en vitesse du vent (à ajuster...)
 
-void handleRoot() {
-  digitalWrite(led, 1);
+float sommeTemperatureBME = 0;
+float sommeHumiditeBME = 0;
+float sommePressionBME = 0;
+float sommeTemperatureBMP = 0;
+float sommePressionBMP = 0;
+int nombreLectures = 0;
 
-  // Lecture des données du capteur BME680
-  float temperature = bme.temperature;
-  float pressure = bme.pressure / 100.0;
-  float humidity = bme.humidity;
-  float gas_resistance = bme.gas_resistance / 1000.0;
-  float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+void gestionInterruption() {
+  compteImpulsions++;  // Incrémenter le compteur à chaque front montant détecté
+}
+// Fonction demandée par monsieur Mazzeo
+void calculerMoyennes() {
+  sommeTemperatureBME += bme.temperature;
+  sommeHumiditeBME += bme.humidity;
+  sommePressionBME += bme.pressure;
+  sommeTemperatureBMP += bmp.temperature;
+  sommePressionBMP += bmp.pressure;
+  nombreLectures++;
+}
 
-  // Construction de la réponse HTML avec les données du capteur
+float obtenirTemperatureMoyenne() {
+  return (sommeTemperatureBME + sommeTemperatureBMP) / (2 * nombreLectures);
+}
+
+float obtenirHumiditeMoyenne() {
+  return (sommeHumiditeBME) / nombreLectures;  // On utilise seulement les données du capteur BME680 pour l'humidité
+}
+
+float obtenirPressionMoyenne() {
+  return (sommePressionBME + sommePressionBMP) / (2 * nombreLectures);
+}
+//*********************************************************************************************************************
+void gererRacine() {
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Lecture des données des capteurs
+  calculerMoyennes();
+  float temperatureBME = bme.temperature;
+  float pressionBME = bme.pressure / 100.0;  // Conversion en hPa
+  float humiditeBME = bme.humidity;
+  float resistanceGazBME = bme.gas_resistance / 1000.0;
+  float altitudeBME = bme.readAltitude(PRESSIONNIVEAUSEALE);
+  float temperatureBMP = bmp.temperature;
+  float pressionBMP = bmp.pressure / 100.0;  // Conversion en hPa
+
+  // Calcul de la vitesse du vent
+  detachInterrupt(digitalPinToInterrupt(brocheEntree));                               // Détacher l'interruption pendant le calcul
+  float vitesseVent = compteImpulsions * facteur;                                     // Calculer la vitesse du vent en utilisant le facteur de conversion
+  compteImpulsions = 0;                                                               // Réinitialiser le compteur de pulses
+  attachInterrupt(digitalPinToInterrupt(brocheEntree), gestionInterruption, RISING);  // Réattacher l'interruption
+
+  // Construction de la réponse HTML avec les données des capteurs
   String page = "<!DOCTYPE html><html><head><title>ESP32 Capteurs</title></head><body>";
-  page += "<h1>Données du capteur BME680 :</h1>";
+  page += "<h1>Données des capteurs :</h1>";
+  page += "<h2>Capteur BME680 :</h2>";
   page += "<ul>";
-  page += "<li>Température BME680: " + String(temperature) + " *C</li>";
-  page += "<li>Pression BME680: " + String(pressure) + " hPa</li>";
-  page += "<li>Humidité BME680: " + String(humidity) + " %</li>";
-  page += "<li>Résistance au gaz BME680: " + String(gas_resistance) + " KOhms</li>";
-  page += "<li>Altitude BME680: " + String(altitude) + " m</li>";
+  page += "<li>Température BME680: " + String(temperatureBME) + " *C</li>";
+  page += "<li>Pression BME680: " + String(pressionBME) + " hPa</li
+
+          > ";
+          page += "<li>Humidité BME680: " + String(humiditeBME) + " %</li>";
+  page += "<li>Résistance au gaz BME680: " + String(resistanceGazBME) + " KOhms</li>";
+  page += "<li>Altitude BME680: " + String(altitudeBME) + " m</li>";
+  page += "</ul>";
+  page += "<h2>Capteur BMP3XX :</h2>";
+  page += "<ul>";
+  page += "<li>Température BMP3XX: " + String(temperatureBMP) + " *C</li>";
+  page += "<li>Pression BMP3XX: " + String(pressionBMP) + " hPa</li>";
+  page += "</ul>";
+  page += "<h2>Moyennes :</h2>";
+  page += "<ul>";
+  page += "<li>Température moyenne: " + String(obtenirTemperatureMoyenne()) + " *C</li>";
+  page += "<li>Pression moyenne: " + String(obtenirPressionMoyenne()) + " hPa</li>";
+  page += "<li>Humidité moyenne: " + String(obtenirHumiditeMoyenne()) + " %</li>";
+  page += "<li>Vitesse du vent : " + String(vitesseVent) + " m/s</li>";
   page += "</ul>";
   page += "</body></html>";
 
   // Envoi de la réponse HTML au client
-  server.send(200, "text/html", page);
+  serveur.send(200, "text/html", page);
 
-  digitalWrite(led, 0);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void setup(void) {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+void configuration() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -58,7 +128,8 @@ void setup(void) {
   // Initialisation du capteur BME680
   if (!bme.begin()) {
     Serial.println("Impossible de trouver le capteur BME680, vérifiez le câblage !");
-    while (1);
+    while (1)
+      ;
   }
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
@@ -66,23 +137,38 @@ void setup(void) {
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150);
 
+  // Initialisation du capteur BMP3XX
+  if (!bmp.begin_I2C()) {
+    Serial.println("Impossible de trouver le capteur BMP3XX, vérifiez le câblage !");
+    while (1)
+      ;
+  }
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
   // Attente de la connexion WiFi
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected to ");
+  Serial.print("Connecté à ");
   Serial.println(ssid);
-  Serial.print("IP address: ");
+  Serial.print("Adresse IP: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", handleRoot);
-  server.begin();
-  Serial.println("HTTP server started");
+  // Configuration de l'interruption pour le comptage des pulses de l'anémomètre
+  pinMode(brocheEntree, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(brocheEntree), gestionInterruption, RISING);
+
+  serveur.on("/", gererRacine);
+  serveur.begin();
+  Serial.println("Serveur HTTP démarré");
 }
 
-void loop(void) {
-  server.handleClient();
+void boucle() {
+  serveur.handleClient();
   delay(2);
 }
